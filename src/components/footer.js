549497df04a7f3911c1cf9e949418e0c673bd6f4
Kate Leonchikova <leonchikova.kate@gmail.com>
2025-02-27 15:09:58 +0100

    footer added

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,19 @@
 import { el } from "redom";
+import copyrightImg from "../assets/images/copyright.svg";
 
 export function renderFooter() {
-  const footer = el("footer", { class: "footer" }, "FOOTER");
+  const currentYear = new Date().getFullYear();
+
+  const footer = el(
+    "footer",
+    { class: "footer" },
+    el(
+      "div",
+      { className: "container footer__container" },
+      el("img", { src: copyrightImg }),
+      el("span", { className: "footer__year" }, currentYear)
+    )
+  );
 
   return footer;
 }
